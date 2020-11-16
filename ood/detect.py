92e2a1f94e9fbb51d4e4ca6a03ca832f62e60ac9
Ethan L. Mines <elmines@crimson.ua.edu>
2020-11-16 08:17:55 -0600

    Use relax_shapes on detect function

--- ood/detect.py
+++ ood/detect.py
@@ -42,40 +42,8 @@ def run_inference(model, preprocessor, label_id_offsets, win_set=None):
 
     return (images_dict, predicted_boxes_list, predicted_classes_list, predicted_scores_list)
 
-#def run_inference(test_images_np: List, test_images_dict: dict, label_id_offsets: dict, 
-#      detection_model: any) -> Tuple[dict, List, List, List]:
-#    """ Runs the detection process on each of the windows in the test set and
-#    stores the results in test_windows_dict
-#    """
-#    test_images_dict = copy_json(test_images_dict) # Don't modify original dict
-#
-#    predicted_boxes_list   = [None] * len(test_images_np)
-#    predicted_classes_list = [None] * len(test_images_np)
-#    predicted_scores_list  = [None] * len(test_images_np)
-#    for image_id, image_info in test_images_dict.items():
-#        for window_id, window_info in image_info['windows'].items():
-#            window = test_images_np[window_id]
-#            input_tensor = tf.convert_to_tensor(np.expand_dims(window, axis=0), dtype=tf.float32)
-#            # run detections
-#            detections = detect(input_tensor, detection_model)
-#            # set predicted detections
-#            predicted_boxes   = detections['detection_boxes'][0].numpy()
-#            predicted_scores = detections['detection_scores'][0].numpy()
-#            # we need to map the indices used for the retraining back to their original category ids
-#            predicted_classes = map_indices_to_category_ids(label_id_offsets, 
-#              detections['detection_classes'][0].numpy().astype(np.uint32).tolist())
-#            # set the results
-#            predicted_boxes_list[window_id]    = predicted_boxes
-#            predicted_classes_list[window_id]  = predicted_classes
-#            predicted_scores_list[window_id]   = predicted_scores
-#            test_images_dict[image_id]['windows'][window_id]['predicted_boxes']   = predicted_boxes
-#            test_images_dict[image_id]['windows'][window_id]['predicted_classes'] = predicted_classes
-#            test_images_dict[image_id]['windows'][window_id]['predicted_scores']  = predicted_scores
-#
-#    return (test_images_dict, predicted_boxes_list, predicted_classes_list, predicted_scores_list)
-
 # Again, uncomment this decorator if you want to run inference eagerly
-@tf.function
+@tf.function(experimental_relax_shapes=True)
 def detect(input_tensor: any, detection_model: any) -> dict:
   """Run detection on an input image.
 
